ac2f8ae74a8a7ac94f339eeb2552087d77255e08
jnglze69 <>
2025-03-08 23:47:07 +0100

    Integracion turso database en la pagina de categorias

--- src/data/repository/category-repository.ts
+++ src/data/repository/category-repository.ts
@@ -1,46 +1,13 @@
-import { turso } from '../../turso';
-import { categories } from '../schema'
 import logger from '../../configuration/logger';
-import { dbInit } from "../drizzle";
 import type { Category } from '../../model/AppModel';
 import { Base64 } from 'js-base64';   
-import { LibSQLDatabase } from "drizzle-orm/libsql";
 
-export async function getCategories(env :Env):Promise<Category[]> {
-    logger.info("getCardCategories.INI");
-    const turso = dbInit(env);
-    const results = await turso.select().from(categories).all();
+export function mapperCategories(resultsTurso):Category[] {
+    
     let elements: Category[] = [];
     
-    if (results.length) {
-        results.forEach((result) => {
-            let element = {
-                id: result.id,
-                name: Base64.decode(result.name),          
-                displayName: Base64.decode(result.name),
-                path: result.path,
-                color: result.color,
-                target: result.target,
-                icon : result.icon,   
-                subTitle : result.subTitle,
-                description: Base64.decode(result.subTitle? result.subTitle:''),
-            } as Category;
-
-            elements.push(element);
-        });
-    }
-    logger.info("getCardCategories Categories:"+JSON.stringify(elements));
-    logger.info("getCardCategories.FIN");
-    return elements;
-}
-
-export async function getCategoriesDb(db :LibSQLDatabase):Promise<Category[]> {
-    logger.info("getCardCategories.INI"); 
-    const results = await db.select().from(categories).all();
-    let elements: Category[] = [];
-    
-    if (results.length) {
-        results.forEach((result) => {
+    if (resultsTurso.length) {
+        resultsTurso.forEach((result) => {
             let element = {
                 id: result.id,
                 name: Base64.decode(result.name),          
